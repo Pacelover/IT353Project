1e8af14e4e3b6c72895d11a600a15bdb364e345e
Pacelover <70454344+Pacelover@users.noreply.github.com>
2022-11-02 17:55:18 -0500

    Step 7 Complete

--- weather_app/weather_app/weather/models.py
+++ weather_app/weather_app/weather/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self): #see city chosen by user
+        return self.name
+
+    class Meta: # show cities instead of city when displaying more than 1
+        verbose_name_plural = 'cities'
--- weather_app/weather_app/weather/views.py
+++ weather_app/weather_app/weather/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 import requests
+from .models import City
 
 # Create your views here.
 def index(request):
     # API
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=60191ca9e0dc4470867884ba38fd64d8'
-    city = 'Las Vegas'
-    # Request API data and convert to json type
-    city_weather = requests.get(url.format(city)).json()
-    print(city_weather)
-    return render(request, 'weather/index.html') # returns the index.html template
+    cities = City.objects.all() # return all cities in db
+    weather_data = []
+    for city in cities:
+        # Request API data and convert to json type
+        city_weather = requests.get(url.format(city)).json()
+        print(city_weather)
+        # dictionary that stores weather data retrieved by user
+        weather = {
+            'city': city,
+            'temperature': city_weather['main']['temp'],
+            'description': city_weather['weather'][0]['description'],
+            'icon': city_weather['weather'][0]['icon']
+        }
+        weather_data.append(weather)
+    context = {'weather_data': weather_data}
+    print(weather)
+    return render(request, 'weather/index.html', context) # returns the index.html template
