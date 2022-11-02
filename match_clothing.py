a416eabf3a7655ba1b23c9be43814e65396701d6
Mateiionescu5963 <79994054+Mateiionescu5963@users.noreply.github.com>
2022-11-02 18:17:20 -0500

    edit via upload

--- match_clothing.py
+++ match_clothing.py
@@ -3,19 +3,22 @@
 # https://en.wikipedia.org/wiki/Clothing_insulation
 
 
-def match_clothing(temp,windchill,humidity):
-    #heat measured in Watts 
-    #measures averaged across humanity
-    activity_to_metabolic_heat = {'resting':103, 'walking':201 ,'jogging':263 , 'sprinting':385}
-    thermal_conductivity_by_temp = {}
+def required_clo(temp,windchill,humidity, meta_heat):
     #human skin surface area ~= 1.75 meters
     transfer_area = 1.75
-    material_thickness = {'skin':0.001, }
     #at 100% humidity, thermal conductivity is amplified by 3% (e.g. 0% hum: 100% therm_cond; 100% hum: 103% therm_cond)
     humidity_amp = 0.03 * (humidity/100.0)
     
+    return (0.155 * temp * transfer_area)/meta_heat
     
     
-    #conductive_heat_transfer = (thermal_conductivity/material_thickness)* transfer_area * 1 (one hour)
     
+def get_clothing(temp,windchill,humidity):
     
+    #heat measured in Watts 
+    activity_to_metabolic_heat = {'resting':103, 'walking':201 ,'jogging':263 , 'sprinting':385}
+    
+    #temp must be in Kelvin
+    temp_in_kelvin = get_kelvin(temp, 'C')
+    
+    clothing_to_clo = {}
