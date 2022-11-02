ef043cb8da5bbcb64cca8008c804af05f610d33c
Pacelover <70454344+Pacelover@users.noreply.github.com>
2022-11-02 17:31:43 -0500

    Step 6 (w/ invalid API)
    API can take up to a couple of hours to be activated, so it currently isn't working)

--- weather_app/weather_app/weather/views.py
+++ weather_app/weather_app/weather/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 def index(request):
+    # API
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=60191ca9e0dc4470867884ba38fd64d8'
+    city = 'Las Vegas'
+    # Request API data and convert to json type
+    city_weather = requests.get(url.format(city)).json()
+    print(city_weather)
     return render(request, 'weather/index.html') # returns the index.html template
