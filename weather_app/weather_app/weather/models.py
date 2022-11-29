4b9ede938c95fa65ab00d7f3e32d570a8724856e
Pacelover <70454344+Pacelover@users.noreply.github.com>
2022-11-29 17:14:52 -0600

    Add start of test

--- weather_app/weather_app/weather/models.py
+++ weather_app/weather_app/weather/models.py
@@ -6,6 +6,3 @@ class City(models.Model):
 
     def __str__(self): #see city chosen by user
         return self.name
-
-    class Meta: # show cities instead of city when displaying more than 1
-        verbose_name_plural = 'cities'
--- weather_app/weather_app/weather/tests.py
+++ weather_app/weather_app/weather/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class TestButtonSubmit(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test(self):
+        self.client.post('', {'name': 'Chicago'})
+        response = self.client.get(reverse('weather_data'))
+        self.assertEqual(response.status_code, 200)
+        print(response)
+        #self.assertEqual(response, 'Chicago')
