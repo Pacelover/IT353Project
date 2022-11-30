cb77afc8c7facc63317f879f2d6381d7afc4b628
Pacelover <70454344+Pacelover@users.noreply.github.com>
2022-11-29 19:37:43 -0600

    Added state to report

--- weather_app/weather_app/weather/views.py
+++ weather_app/weather_app/weather/views.py
@@ -16,14 +16,30 @@ def index(request):
     form = CityForm()
     # Request API data and convert to json type
     city = requests.get(url.format(cityName)).json()
-    #print(city)
-    # dictionary that stores weather data retrieved by user
-    weather = {
-        'city': city['name'],
-        'temperature': city['main']['temp'],
-        'description': city['weather'][0]['description'],
-        'icon': city['weather'][0]['icon']
-    }
-    context = {'weather': weather, 'form': form}
+    try:
+        lon = city['coord']['lon']
+        lat = city['coord']['lat']
+        # print(city)
+        url = 'http://api.openweathermap.org/geo/1.0/reverse?lat={}&lon={}&limit=5&appid=60191ca9e0dc4470867884ba38fd64d8'
+        location = requests.get(url.format(lat, lon)).json()
+        state = location[0]['state']
+        print("Location: ", state)
+        # dictionary that stores weather data retrieved by user
+        weather = {
+            'city': city['name'] + ', ' + state,
+            'temperature': city['main']['temp'],
+            'description': city['weather'][0]['description'],
+            'icon': city['weather'][0]['icon']
+        }
+    except:
+        print("City not found")
+        weather = {
+            'city': '',
+            'state': '',
+            'temperature': '',
+            'description': '',
+            'icon': ''
+        }
     #print(weather)
+    context = {'weather': weather, 'form': form}
     return render(request, 'weather/index.html', context) # returns the index.html template
