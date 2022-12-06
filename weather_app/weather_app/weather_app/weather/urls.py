c89ec431cf04eb4920f6f093d34bbb20b25414d8
Pacelover <70454344+Pacelover@users.noreply.github.com>
2022-12-06 15:49:31 -0600

    Revert "Add files via upload"
    This reverts commit bfc588659e821ffda46df56576e2b8a9df133890.

--- weather_app/weather_app/weather_app/weather/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='weather_data'),
-    path('<int:weather_id>/delete', views.delete, name='delete'),
-    path('<int:weather_id>/update', views.update, name='update'),
-    path('add/', views.add, name='add')
-]
